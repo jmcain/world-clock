494a2bbf130d0c893266fcb229418b1190f9652c
jcain <jessiecain.au@gmail.com>
2026-02-03 18:19:45 +0800

    moment install, JS work on time udpates

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function updateLosAngelesTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format("h:mm:ss")}
+<small>${losAngelesTime.format("A")}<small>`;
+}
+
+updateLosAngelesTime();
+setInterval(updateLosAngelesTime, 1000);
+
+function updateSydneyTime() {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+
+  sydneyTimeElement.innerHTML = `${sydneyTime.format("h:mm:ss")}
+<small>${sydneyTime.format("A")}<small>`;
+}
+updateSydneyTime();
+setInterval(updateSydneyTime, 1000);
